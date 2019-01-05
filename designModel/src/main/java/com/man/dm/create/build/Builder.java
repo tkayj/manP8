ff777f29e1365684341f0565c9133a66517f013a
tkayj <2003tianke@163.com>
2019-01-05 11:45:18 +0800

    1234

--- designModel/src/main/java/com/man/dm/create/build/Builder.java
+++ designModel/src/main/java/com/man/dm/create/build/Builder.java
@@ -3,8 +3,7 @@ package com.man.dm.create.build;
 public interface Builder {
 
 	public void buildPart1();
-	public void buildPart2();
-	
+	public void buildPart2();	
 	public Product retrieveResult();
 	
 }
