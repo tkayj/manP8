f38c92a8bf6613626ae3dc4eb6dbc4baa25871b1
tkayj <2003tianke@163.com>
2019-01-07 13:08:37 +0800

    更改

--- designModel/src/main/java/com/man/dm/struct/adapter/Target.java
+++ designModel/src/main/java/com/man/dm/struct/adapter/Target.java
@@ -3,12 +3,12 @@ package com.man.dm.struct.adapter;
 public interface Target {
 	
 	 /**
-     * 这是源类Adaptee也有的方法
+          * 这是源类Adaptee也有的方法
      */
     public void sampleOperation1();
     
     /**
-     * 这是源类Adapteee没有的方法
+            * 这是源类Adapteee没有的方法
      */
     public void sampleOperation2();
     
