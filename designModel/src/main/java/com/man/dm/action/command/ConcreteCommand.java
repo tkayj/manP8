d63a4096ca69485d87d85a333b7af9599a39d392
tkayj <2003tianke@163.com>
2019-03-02 18:24:30 +0800

    2	w1

---
+++ designModel/src/main/java/com/man/dm/action/command/ConcreteCommand.java
@@ -0,0 +1,14 @@
+package com.man.dm.action.command;
+
+public class ConcreteCommand implements Command{
+
+	
+	
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
